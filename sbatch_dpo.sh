7780121c87daff0cff45609619d6a44f22684ec1
Jiaxiang Li <li003755@ahl03.agate.msi.umn.edu>
2024-02-13 14:48:20 -0600

    update sh file for job submission

---
+++ sbatch_dpo.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+#SBATCH --time=24:00:00
+#SBATCH --nodes=1
+#SBATCH --mem=62gb
+#SBATCH --output=log/%j.out                              
+#SBATCH --error=log/%j.out
+#SBATCH --job-name=mezo
+#SBATCH -n 12
+#SBATCH --requeue
+#SBATCH --gres=gpu:a100:1
+#SBATCH -p a100-4
+
+source activate dpo
+nvidia-smi
+
+cd ~/jobsubmit/direct-preference-optimization || exit
+python -u train.py model=pythia69 datasets=[hh] loss=sft exp_name=anthropic_dpo_pythia69 gradient_accumulation_steps=2 batch_size=64 eval_batch_size=32 trainer=FSDPTrainer sample_during_eval=false
+
+exit
