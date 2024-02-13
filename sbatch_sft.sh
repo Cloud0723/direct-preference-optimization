bc21500aef1471a2d7146bd3a275511fe0a4a587
JasonJiaxiangLi <jasonljx96@gmail.com>
2024-02-13 15:10:15 -0600

    update sh file for msi job

--- sbatch_sft.sh
+++ sbatch_sft.sh
@@ -15,5 +15,6 @@ source activate dpo
 nvidia-smi
 
 cd ~/jobsubmit/direct-preference-optimization || exit
-python -u train.py model=pythia69 datasets=[hh] loss=dpo loss.beta=0.1 model.archive=/path/to/checkpoint/from/sft/step-XXXX/policy.pt exp_name=anthropic_dpo_pythia69 gradient_accumulation_steps=2 batch_size=32 eval_batch_size=32 trainer=FSDPTrainer sample_during_eval=false
+python -u train.py model=pythia69 datasets=[hh] loss=sft exp_name=anthropic_dpo_pythia69 gradient_accumulation_steps=2 batch_size=64 eval_batch_size=32 trainer=FSDPTrainer sample_during_eval=false
+
 exit
